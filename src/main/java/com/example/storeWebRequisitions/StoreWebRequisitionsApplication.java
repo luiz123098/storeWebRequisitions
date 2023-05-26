61c2a65ca650299a2c586776374e3f2756191577
Neto <luiznetopaluti@hotmail.com>
2023-05-26 06:37:35 -0300

    Initial commit

---
+++ src/main/java/com/example/storeWebRequisitions/StoreWebRequisitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeWebRequisitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebRequisitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWebRequisitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storeWebRequisitions/StoreWebRequisitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storeWebRequisitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWebRequisitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
